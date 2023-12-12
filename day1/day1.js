86002b9d9f96562c365a71a458f87f438623ec07
deucenn <panix.black@web.de>
2023-12-12 15:47:28 +0100

    day1 part2 done

--- day1/day1.js
+++ day1/day1.js
@@ -1,22 +1,49 @@
-const { text } = require('./input.js')
-const input = text
-const linesArr = input.split('\n').map(line => line.replace(/[^0-9]/g, ''));
+const calculateTotalCount = (linesArr) => {
+  let totalCount = 0;
 
-let totalCount = 0;
-
-for (let i = 0; i < linesArr.length; i++) {
-    const currentLine = linesArr[i];
+  for (let i = 0; i < linesArr.length; i++) {
+    let currentLine = linesArr[i];
 
     if (currentLine.length > 0) {
-        const firstNumber = Number(currentLine[0]);
-        const lastNumber = Number(currentLine[currentLine.length - 1]);
+      const firstNumber = Number(currentLine[0]);
+      const lastNumber = Number(currentLine[currentLine.length - 1]);
 
-        const concatenatedNumber = Number(`${firstNumber}${lastNumber}`);
+      const concatenatedNumber = Number(`${firstNumber}${lastNumber}`);
 
-        totalCount += concatenatedNumber;
+      totalCount += concatenatedNumber;
     }
+  }
+
+  return totalCount;
+};
+
+const numbers = {
+  one: "o1e",
+  two: "t2o",
+  three: "t3e",
+  four: "f4r",
+  five: "f5e",
+  six: "s6x",
+  seven: "s7n",
+  eight: "e8t",
+  nine: "n9e",
+};
+
+const { text } = require("./input.js");
+const input = text;
+let linesArr = input.split("\n");
+
+// Replace words with numbers
+for (let key in numbers) {
+  linesArr = linesArr.map((line) =>
+    line.replace(new RegExp(key, "g"), numbers[key])
+  );
 }
 
-console.log(totalCount);
+// Remove non-numeric characters
+linesArr = linesArr.map((line) => line.replace(/[^0-9]/g, ""));
 
+const result = calculateTotalCount(linesArr);
+console.log(result);
 
+console.log(linesArr);
